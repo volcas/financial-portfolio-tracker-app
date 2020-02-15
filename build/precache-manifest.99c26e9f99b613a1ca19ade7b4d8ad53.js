8f152bead59bc5e753e49bc3e9aa4d239864736d
Nikhil Nair <nnair25@gmail.com>
2020-02-15 11:51:08 +0530

    Added correct solution

---
+++ build/precache-manifest.99c26e9f99b613a1ca19ade7b4d8ad53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8460708ddd0d9b18b69231320e47dceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0dcf855532dbba94b9a",
+    "url": "/static/css/main.e3e68735.chunk.css"
+  },
+  {
+    "revision": "a3a4ac76dad502564c67",
+    "url": "/static/js/2.d0382ed4.chunk.js"
+  },
+  {
+    "revision": "b0dcf855532dbba94b9a",
+    "url": "/static/js/main.9a09c8d4.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
